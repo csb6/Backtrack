9cee2e3822082e16b5ffbec1fe7ff20a7c4274ea
csb6 <cblakley15@gmail.com>
2019-10-31 17:24:37 -0700

    Added support for facts, fixed bug with finding solutions, added header comment explaining purpose of program

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
-/* TODO;
- - Generalize Facts as predicates or make Facts a subclass of
-   Predicate, or both subclasses of abstract class??
+/* Author: Cole Blakley
+   File: main.cpp
+   Purpose: The program is a sort of Prolog within C++. You create a Logic
+     Machine with three type parameters: The type for the names of facts and
+     predicates (usually an enum class or string), and two other types, A and
+     B, representing the two types of data that can be logically-related within
+     facts/predicates. To get started, instantiate a LogicMachine<> with
+     these 3 types, in order, parameterized in the type.
+
+     Facts must (currently) be in form `factname(A a, B b)`. They represent
+     a concrete association between 2 values, a and b. To add a Fact to a
+     LogicMachine instance, do `lm.add(factName, new Fact<A,B>(aVal, bVal))`.
+
+     Rules are essentially function objects that can take in variable numbers
+     of arguments. Using a given function pointer that returns true or false,
+     a rule will match or not match with a given query. To make a new rule,
+     first define a function/lambda, and pass its name to
+     ln.add(ruleName, new Rule(decoder,fcnPtr)). decoder is a string consisting
+     of lowercase 'a' and 'b' representing the argument order and type that
+     the rule's function ptr accepts. For example, a function that needs an
+     A value and a B value would have a decoder string "ab".
 */
 #include <iostream>
 #include <vector>
@@ -10,24 +28,21 @@
 #include <cstdarg> // for ellipsis
 #include <functional>
 
-template<typename N, typename A, typename B>
 class Truth {
 public:
-    virtual N name() = 0;
-    virtual bool trueGiven(A a, B b) = 0;
-    virtual bool matches(const std::string decoder, ...);
+    virtual bool matches(std::string decoder, va_list args) = 0;
     virtual ~Truth() {};
 };
 
-class Predicate {
+class Rule : public Truth {
 private:
     std::string m_decoder;
     bool(*m_pred)(std::string,va_list);
 public:
-    Predicate(std::string decoder, bool(*pred)(std::string,va_list))
+    Rule(std::string decoder, bool(*pred)(std::string,va_list))
 	: m_decoder(decoder), m_pred(pred)
     {}
-    bool operator()(std::string decoder, va_list args)
+    virtual bool matches(std::string decoder, va_list args) override
     {
 	if(decoder != m_decoder) {
 	    return false;   
@@ -36,48 +51,28 @@ public:
     }
 };
 
-template<typename N, typename A, typename B>
-class Rule {
+template<typename A, typename B>
+class Fact : public Truth {
 private:
-    N m_name;
-    Predicate m_pred;
-public:
-    Rule(const N name, Predicate pred)
-	: m_name(name), m_pred(pred)
-    {}
-
-    N name()
-    {
-	return m_name;
-    }
-
-    bool matches(const std::string decoder, va_list args)
-    {
-	return m_pred(decoder, args);
-    }
-};
-
-/*template<typename N, typename A, typename B>
-class Fact : public Truth<N,A,B> {
-private:
-    N m_name;
     A m_a;
     B m_b;
 public:
-    Fact(const N name, const A a, const B b)
-	: m_name(name), m_a(a), m_b(b)
+    Fact(A a, B b) : m_a(a), m_b(b)
     {}
-
-    virtual N name() override
+    virtual bool matches(std::string decoder, va_list args) override
     {
-	return m_name;
+	if(decoder != "ab") {
+	    return false;  
+	}
+	//Have to copy args since taking items out of args is destructive
+	va_list argsCopy;
+	va_copy(argsCopy, args);
+	A a = va_arg(argsCopy, A);
+	B b = va_arg(argsCopy, B);
+	va_end(argsCopy);
+        return a == m_a && b == m_b;
     }
-
-    virtual bool trueGiven(A a, B b) override
-    {
-	return m_a == a && m_b == b;
-    }
-    };*/
+};
 
 
 template<typename N, typename A, typename B>
@@ -85,24 +80,16 @@ class LogicMachine {
     static_assert(!std::is_same<N,A>::value, "Name type must differ from A type");
     static_assert(!std::is_same<N,B>::value, "Name type must differ from B type");
 private:
-    using Rule_t = Rule<N,A,B>;
-    std::map<N,std::vector<Rule_t*>> m_truths;
-    unsigned int m_candidate_index = 0;
-
-    Rule_t* getCandidates(const N truthName)
+    std::map<N,std::vector<Truth*>> m_truths;
+    
+    Truth* getCandidates(unsigned int index, const N truthName)
     {
-	if(m_truths.find(truthName) == m_truths.end()) {
+	if(m_truths.find(truthName) == m_truths.end()
+	   || index >= m_truths[truthName].size()) {
 	    // If key doesn't exist, fact can't be found
 	    return nullptr;
 	}
-	for(auto i=m_candidate_index; i<m_truths.size(); ++i) {
-	    Rule_t* truth = m_truths[truthName][i];
-	    if(truth != nullptr && truth->name() == truthName) {
-		m_candidate_index = i + 1;
-		return truth;
-	    }
-	}
-	return nullptr;
+	return m_truths[truthName][index];
     }
 public:
     ~LogicMachine()
@@ -113,27 +100,29 @@ public:
 	    }
 	}
     }
-    
-    /*void add(const N factName, const A aValue, const B bValue)
-    {
-	m_truths[factName].push_back(new Fact_t(factName, aValue, bValue));
-	}*/
 
-    void add(const N ruleName, Predicate pred)
+    void add(const N factName, Fact<A,B> *fact)
     {
-	m_truths[ruleName].push_back(new Rule_t(ruleName, pred));
+	m_truths[factName].push_back(fact);
+    }
+
+    void add(const N ruleName, Rule *pred)
+    {
+	m_truths[ruleName].push_back(pred);
     }
 
     bool isTrue(const N truthName, std::string decoder, ...)
     {
 	va_list args;
 	va_start(args, decoder);
-	Rule_t* truth = nullptr;
-	while((truth = getCandidates(truthName)) != nullptr) {
+	Truth* truth = nullptr;
+	unsigned int index = 0;
+	while((truth = getCandidates(index, truthName)) != nullptr) {
 	    if(truth->matches(decoder, args)) {
 		va_end(args);
 		return true;
 	    }
+	    ++index;
 	}
 	va_end(args);
 	return false;
@@ -187,13 +176,19 @@ int main()
     LogicMachine<FactName, Planet, Star> db;
 
     //Intiial Facts
-    //db.add(FactName::Orbits, Planet::Mercury, Star::Sun);
-    //db.add(FactName::Orbits, Planet::Venus, Star::Sun);
-    //db.add(FactName::Orbits, Planet::Earth, Star::Sun);
+    db.add(FactName::Orbits, new Fact<Planet,Star>(Planet::Mercury, Star::Sun));
+    db.add(FactName::Orbits, new Fact<Planet,Star>(Planet::Venus, Star::Sun));
+    db.add(FactName::Orbits, new Fact<Planet,Star>(Planet::Earth, Star::Sun));
 
-    //std::cout << db.isTrue(FactName::Orbits, Planet::Earth, Star::AlphaCentauri) << '\n';
+    std::cout << db.isTrue(FactName::Orbits, "ab", Planet::Earth, Star::Sun)
+	      << '\n';
 
-    db.add(FactName::Truth, Predicate("ab", alwaysTrue));
-    std::cout << db.isTrue(FactName::Truth, "aba", Planet::Mercury, Star::Sun) << '\n';
+    //Predicates
+    db.add(FactName::Truth, new Rule("ab", alwaysTrue));
+    
+    std::cout << db.isTrue(FactName::Truth, "ab", Planet::Mercury, Star::Sun) << '\n';
+
+    std::cout << db.isTrue(FactName::Orbits, "ab", Planet::Venus, Star::AlphaCentauri)
+	      << '\n';
     return 0;
 }
